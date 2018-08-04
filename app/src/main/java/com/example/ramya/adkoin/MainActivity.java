51203f4cd6d551f81a03f1f9c036c9ec7a911533
kavi <kavithav0509@gmail.com>
2018-08-04 19:56:04 +0530

    Second commit

--- app/src/main/java/com/example/ramya/adkoin/MainActivity.java
+++ app/src/main/java/com/example/ramya/adkoin/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.ramya.adkoin;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.os.Message;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -26,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
     private final static int REQUEST_CODE=999;
 
-   // SharedPreferences sp;
+    Button mobile;
+
+    SharedPreferences sp;
 
 
     @Override
@@ -34,38 +39,82 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_PHONE_STATE)){
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                        Manifest.permission.READ_PHONE_STATE}, 1);
+
+            } else {
+
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                        Manifest.permission.READ_PHONE_STATE}, 1);
+
+            }
+        } else {
+
+        }
+
         printKeyHash();
 
+        mobile=(Button)findViewById(R.id.mobile);
 
-        //sp = getSharedPreferences("login",MODE_PRIVATE);
+        sp = getSharedPreferences("login",MODE_PRIVATE);
 
-        startLoginPage(LoginType.PHONE);
-        //sp.edit().putBoolean("logged",true).apply();
+        mobile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startLoginPage(LoginType.PHONE);
+                sp.edit().putBoolean("logged",true).apply();
+
+            }
+        });
 
 
-        /*if(sp.getBoolean("logged",false)){
+        if(sp.getBoolean("logged",false)) {
             goTosuccess();
-        }*/
+        }
+
+
     }
 
-    /*private void goTosuccess() {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "permission Granted", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "No permission Granted", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    private void goTosuccess() {
         Intent i = new Intent(this,Success.class);
         startActivity(i);
-    }*/
+    }
 
     private void startLoginPage(LoginType loginType) {
-
+        if (loginType==LoginType.PHONE) {
 
             Intent intent = new Intent(this, AccountKitActivity.class);
-            AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder=
+            AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
                     new AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.PHONE,
                             AccountKitActivity.ResponseType.CODE);
 
-            intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,configurationBuilder.build());
+            intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configurationBuilder.build());
 
-            startActivityForResult(intent,REQUEST_CODE);
+            startActivityForResult(intent, REQUEST_CODE);
 
-
+        }
 
 
 
@@ -74,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
 
         if (requestCode==REQUEST_CODE){
             AccountKitLoginResult result = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
@@ -93,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
                 else
                     Toast.makeText(this,"Success! +%s"+result.getAuthorizationCode().substring(0,10),Toast.LENGTH_SHORT).show();
 
-                startActivity(new Intent(this,Success.class));
+                startActivity(new Intent(this, Success.class));
             }
         }
 
@@ -116,4 +166,6 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+
 }
--- app/src/main/java/com/example/ramya/adkoin/Success.java
+++ app/src/main/java/com/example/ramya/adkoin/Success.java
@@ -1,32 +1,81 @@
 package com.example.ramya.adkoin;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.EditText;
-
-import com.facebook.accountkit.Account;
-import com.facebook.accountkit.AccountKit;
-import com.facebook.accountkit.AccountKitCallback;
-import com.facebook.accountkit.AccountKitError;
+import android.widget.FrameLayout;
 
 public class Success extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private Cashout cashfragment;
+    private Balance balfragment;
+    private Banners banfragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
 
-       /* Button SignOut = (Button)findViewById(R.id.logout);
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.navbot);
 
-        SignOut.setOnClickListener(new View.OnClickListener() {
+        cashfragment = new Cashout();
+        balfragment = new Balance();
+        banfragment = new Banners();
+
+        setFragment(cashfragment);
+
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                AccountKit.logOut();
-                finish();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+
+                    case R.id.nav_cash:
+                        mMainNav.setItemBackgroundResource(R.color.basic_blue);
+                        setFragment(cashfragment);
+                        return true;
+
+                    case R.id.nav_bal:
+                        mMainNav.setItemBackgroundResource(R.color.basic_blue);
+                        setFragment(balfragment);
+                        return true;
+
+                    case R.id.nav_ban:
+                        mMainNav.setItemBackgroundResource(R.color.basic_blue);
+                        setFragment(banfragment);
+                        return true;
+
+                    case R.id.nav_more:
+                        mMainNav.setItemBackgroundResource(R.color.basic_blue);
+                        setFragment(cashfragment);
+                        return true;
+
+
+                    default:return false;
+
+                }
+
             }
-        });*/
+        });
+
     }
+
+    private void setFragment(android.support.v4.app.Fragment fragment) {
+
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+
+    }
+
 
 }
